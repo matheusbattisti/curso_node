374b6c233fcb4ac0e0d7a6ea979c3583a9c76a15
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-08-25 20:04:14 -0300

    finalizando assoc

---
+++ 9_SEQUELIZE/12_delete_relacionados/index.js
@@ -0,0 +1,166 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+const Address = require('./models/Address')
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  User.findAll({ raw: true })
+    .then((users) => {
+      console.log(users)
+      res.render('home', { users: users })
+    })
+    .catch((err) => console.log(err))
+})
+
+app.get('/users/create', function (req, res) {
+  res.render('adduser')
+})
+
+app.post('/users/create', function (req, res) {
+  const name = req.body.name
+  const occupation = req.body.occupation
+  let newsletter = req.body.newsletter
+
+  if (newsletter === 'on') {
+    newsletter = true
+  }
+
+  User.create({ name, occupation, newsletter })
+    .then(res.redirect('/'))
+    .catch((err) => console.log(err))
+})
+
+app.get('/users/:id', function (req, res) {
+  const id = req.params.id
+
+  User.findOne({
+    raw: true,
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      console.log(user)
+      res.render('userview', { user })
+    })
+    .catch((err) => console.log(err))
+})
+
+app.post('/users/delete/:id', function (req, res) {
+  const id = req.params.id
+
+  User.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      res.redirect('/')
+    })
+    .catch((err) => console.log(err))
+})
+
+app.get('/users/edit/:id', function (req, res) {
+  const id = req.params.id
+
+  User.findOne({
+    include: Address,
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      res.render('useredit', { user: user.get({ plain: true }) })
+    })
+    .catch((err) => console.log(err))
+})
+
+app.post('/users/update', function (req, res) {
+  const id = req.body.id
+  const name = req.body.name
+  const occupation = req.body.occupation
+  let newsletter = req.body.newsletter
+
+  if (newsletter === 'on') {
+    newsletter = true
+  } else {
+    newsletter = false
+  }
+
+  const userData = {
+    id,
+    name,
+    occupation,
+    newsletter,
+  }
+
+  console.log(req.body)
+  console.log(userData)
+
+  User.update(userData, {
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      console.log(user)
+      res.redirect('/')
+    })
+    .catch((err) => console.log(err))
+})
+
+app.post('/address/create', function (req, res) {
+  const UserId = req.body.UserId
+  const street = req.body.street
+  const number = req.body.number
+  const city = req.body.city
+
+  const address = {
+    street,
+    number,
+    city,
+    UserId,
+  }
+
+  Address.create(address)
+    .then(res.redirect(`/users/edit/${UserId}`))
+    .catch((err) => console.log(err))
+})
+
+app.post('/address/delete/', function (req, res) {
+  const id = req.body.id
+
+  Address.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(res.redirect('/'))
+    .catch((err) => console.log(err))
+})
+
+// Criar tabelas e rodar o app
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
