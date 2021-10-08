5326258a1b594ffa42bb130aebf6cda809b982b0
Matheus Battisti <matheus.battisti@essentia.com.br>
2021-10-08 13:11:36 -0300

    continuando upload img

--- 15_GETAPET/backend/helpers/image-upload.js
+++ 15_GETAPET/backend/helpers/image-upload.js
@@ -1,25 +1,31 @@
-const multer = require('multer')
+const multer = require("multer");
+const path = require("path");
 
 // Destination to store image
 const imageStorage = multer.diskStorage({
-  destination: 'images',
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + '_' + Date.now() + path.extname(file.originalname),
-    )
+  destination: function (req, file, cb) {
+    let folder = "";
+    if (req.user) {
+      folder = "users";
+    } else if (req.pet) {
+      folder = "pets";
+    }
+    cb(null, `public/images/${folder}/`);
   },
-})
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
 const imageUpload = multer({
   storage: imageStorage,
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(png|jpg)$/)) {
       // upload only png and jpg format
-      return cb(new Error('Por favor, envie apenas png ou jpg!'))
+      return cb(new Error("Por favor, envie apenas png ou jpg!"));
     }
-    cb(undefined, true)
+    cb(undefined, true);
   },
-})
+});
 
-module.exports = { imageUpload }
+module.exports = { imageUpload };
--- 15_GETAPET/backend/routes/UserRoutes.js
+++ 15_GETAPET/backend/routes/UserRoutes.js
@@ -4,11 +4,17 @@ const UserController = require("../controllers/UserController");
 
 // middlewares
 const verifyToken = require("../helpers/check-token");
+const { imageUpload } = require("../helpers/image-upload");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
 router.get("/:id", UserController.getUserById);
-router.patch("/edit/:id", verifyToken, UserController.editUser);
+router.patch(
+  "/edit/:id",
+  verifyToken,
+  imageUpload.single("image"),
+  UserController.editUser
+);
 
 module.exports = router;
